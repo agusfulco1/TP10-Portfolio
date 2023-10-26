dde79dd3842e37dfe967bf4ae309649e7d53af4d
JULIAN CARABALLO <46603069@ort>
2023-10-26 09:35:30 -0300

    context y aboutus teglus afk

--- tp10/src/App.js
+++ tp10/src/App.js
@@ -6,8 +6,29 @@ import AboutUs from './Screens/AboutUs';
 import Creaciones from './Screens/Creaciones';
 import DetalleCreaciones from './Screens/DetalleCreaciones';
 import Favoritos from './Screens/Favoritos';
+import { favoritoContext}  from './Contexts/favoritoContext';
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [favorito,setFavorito] = useState([]);
+  const añadirCreacion = (crea) => {
+    let yaExiste = favorito.findIndex(creacion => creacion.id === crea.id);
+    if(yaExiste === -1) SetCarrito([...favproto, {...crea, cantidad: 1}]);
+    else {
+      setFavorito(favorito.map(creacion => creacion.id === crea.id ? {...creacion, cantidad: creacion.cantidad + 1} : creacion));
+    }
+  }
+
+  const actualizarCantidad = (crea) => {
+      setFavorito(favorito.map(creacion => creacion.id === crea.id ? {...creacion, cantidad: crea.cantidad} : creacion));
+  }
+  const eliminarCreacion = (crea) => {
+    console.log("eliminar:", crea)
+    setFavorito(
+      favorito.filter( creation => creation.id !== crea.id )
+    );}
   return (
+    <carritoContext.Provider value={{favorito, setFavorito, añadirCreacion, actualizarCantidad, eliminarCreacion}}>    
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
@@ -19,6 +40,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+      </carritoContext.Provider>
   );
 }
 
