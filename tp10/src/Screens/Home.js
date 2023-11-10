f2777b6706d2693e8e4a978267b3abecc511eeb1
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-10 08:56:03 -0300

    nada

--- tp10/src/Screens/Home.js
+++ tp10/src/Screens/Home.js
@@ -1,6 +1,18 @@
 import '../Styles/Home.css'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useState } from 'react'
 export default function Home() {
-
+    const [creacionesArr, setCreaciones] =  useState([])
+    useEffect(() => {
+        let array = []
+        axios.get('http://localhost:3000/creaciones.json')
+        .then((response) => {
+            array = response.data
+            console.log(response.data)
+        })
+    }, [])
+    
     return (
         <div className="background">
             <h2>Hola</h2>
