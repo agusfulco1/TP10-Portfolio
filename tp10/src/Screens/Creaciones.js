488fdd0eda648d4e9142f1654e37f55921ab6870
JULIAN CARABALLO <46603069@ort>
2023-11-10 08:52:05 -0300

    Creaciones codeado no puedo ver si anda bien HASTA QUE TEGLUS SE DIGNE EN HACER LAS CREACIONES

--- tp10/src/Screens/Creaciones.js
+++ tp10/src/Screens/Creaciones.js
@@ -1,9 +1,28 @@
+import '../Styles/Home.css'
+import axios from 'axios';
+import { useState, useEffect, useContext, Container } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Creacion from '../Components/Creacion';
 
 export default function Creaciones() {
-
+    const [creaciones, setCreaciones] = useState([{}]);
+    useEffect(() => {
+        axios.get('http://localhost:3000/creaciones.json')
+            .then(res => {
+                console.log(res)
+                setCreaciones(res.data)
+            })
+            .catch(e => {
+                console.log(e)
+            });
+    }, []);
     return (
-        <div>
-            
-        </div>
+        <Container fluid>
+             <Row style={{ padding: '2%' }}>
+                    {creaciones.map(creacion => <Col sm={2}><Creacion titulo={creacion.title} img={creacion.imagen} texto={creaciones.descripcion}  id={creacion.id}></Creacion>
+                    </Col>)}
+                </Row>
+        </Container>
+
     )
 }
