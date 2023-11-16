90b6f50becb8c514522c45861350a96db7bd231c
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-16 09:09:57 -0300

    verdetalle

--- tp10/src/Screens/DetalleCreaciones.js
+++ tp10/src/Screens/DetalleCreaciones.js
@@ -1,9 +1,17 @@
 
+import { useLocation } from "react-router-dom"
 export default function DetalleCreaciones() {
+    const location = useLocation();
+    const objetoRecibido = location.state;
+    console.log("lo que recibo:", objetoRecibido)
 
     return (
-        <div>
-            
-        </div>
+        <div className='container'>
+                <h3>Nombre del producto: {objetoRecibido.titulo}</h3>
+                <img src={objetoRecibido.imagen} alt=''></img>
+                <h3>Descripcion: {objetoRecibido.descripcion}</h3>
+                <h2>fecha: {objetoRecibido.fecha} </h2>
+                <h2>Url: {objetoRecibido.url} </h2>
+    </div>
     )
 }
